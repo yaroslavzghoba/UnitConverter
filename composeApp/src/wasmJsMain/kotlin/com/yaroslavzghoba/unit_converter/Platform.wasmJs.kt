dbf75c18c8104c8f6814781f323a322a43d13833
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2024-10-16 01:01:19 +0200

    Deleted the file with an implementation of a non-existent API

--- composeApp/src/wasmJsMain/kotlin/com/yaroslavzghoba/unit_converter/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.yaroslavzghoba.unit_converter
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
